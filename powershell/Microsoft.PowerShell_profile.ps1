17e31c2a22637ec9ac09e5b8cb59e8441351df7d
zeroicey <zeroicey.hp@outlook.com>
2025-07-11 11:54:11 +0800

    feat(powershell): 添加PowerShell配置文件并初始化starship和zoxide

---
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,13 @@
+Invoke-Expression (&starship init powershell)
+
+function y {
+    $tmp = [System.IO.Path]::GetTempFileName()
+    yazi $args --cwd-file="$tmp"
+    $cwd = Get-Content -Path $tmp -Encoding UTF8
+    if (-not [String]::IsNullOrEmpty($cwd) -and $cwd -ne $PWD.Path) {
+        Set-Location -LiteralPath ([System.IO.Path]::GetFullPath($cwd))
+    }
+    Remove-Item -Path $tmp
+}
+
+Invoke-Expression (& { (zoxide init powershell | Out-String) })
